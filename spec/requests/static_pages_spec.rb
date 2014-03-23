3951d588b440054ce49e01ad4f49eb4ff6abf055
Vagios Vlachos <bagios234@gmail.com>
2014-03-23 21:05:01 +0200

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,61 +1,49 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  subject { page }
 
   describe "Home page" do
+    before {visit root_path}
 
-    it "should have the content 'Yada App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Yada App')
-    end
-
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title('Yada App')
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('- Home')
-    end
+    it { should have_content('Yada App') }
+    it { should have_title('Yada App') }
+    it { should_not have_title('- Home') }
   end
 
   describe "Help page" do
+    before {visit help_path}
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-
-    it "should have title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title('Yada App - Help')
-    end
+    it { should have_content('Help') }
+    it { should have_title('Yada App - Help') }
   end
 
   describe "About page" do
+    before { visit about_path }
 
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
-
-    it "should have title 'About'" do
-      visit '/static_pages/about'
-      expect(page).to have_title('Yada App - About')
-    end
+    it { should have_content('About Us') }
+    it { should have_title('Yada App - About') }
   end
 
   describe "Contact page" do
+    before { visit contact_path }
 
-    it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
-      expect(page).to have_content('Contact')
-    end
+    it { should have_content('Contact') }
+    it { should have_title('Yada App - Contact') }
+  end
 
-    it "should have title 'Contact'" do
-      visit '/static_pages/contact'
-      expect(page).to have_title('Yada App - Contact')
-    end
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title('Yada App - About')
+    click_link "Help"
+    expect(page).to have_content('Help')
+    click_link "Contact"
+    expect(page).to have_content('Contact')
+    click_link "Home"
+    click_link "Sign up"
+    expect(page).to have_content('Sign up')
+    click_link "sample app"
+    expect(page).to have_content('Yada')
   end
 end
